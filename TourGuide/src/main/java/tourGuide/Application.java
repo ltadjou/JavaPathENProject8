41f9357b3a24134b7bb7b9ad3dc38a42fb807a66
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-05-07 10:46:27 +0200

    Update Application main method & create MultiThreading class

--- TourGuide/src/main/java/tourGuide/Application.java
+++ TourGuide/src/main/java/tourGuide/Application.java
@@ -7,7 +7,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+
+        MultiThreading multiThreading = new MultiThreading();
+        multiThreading.start();
+
+//        MultiThreading multiThreading2 = new MultiThreading();
+//        multiThreading2.start();
+
+        /* On peut également faire une boucle */
+//        for (int i=0; i<5; i++){
+//            MultiThreading multiThreading = new MultiThreading();
+//            multiThreading.start();
+//        }
+        
     }
 
 }
---
+++ TourGuide/src/main/java/tourGuide/MultiThreading.java
@@ -0,0 +1,10 @@
+package tourGuide;
+
+public class MultiThreading extends Thread{
+
+    @Override
+    public void run(){
+
+    }
+
+}
