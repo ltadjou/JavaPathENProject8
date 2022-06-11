5e3bad3a50ec86f6a239b744ea4451ee7d05d51a
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-06-11 11:59:43 +0200

    tout deplacer à la racine

---
+++ src/main/java/tourGuide/TourGuideController.java
@@ -0,0 +1,137 @@
+package tourGuide;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.jsoniter.annotation.JsonObject;
+import gpsUtil.location.Attraction;
+import jdk.nashorn.api.scripting.JSObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jsoniter.output.JsonStream;
+
+import gpsUtil.location.VisitedLocation;
+import tourGuide.service.TourGuideService;
+import tourGuide.user.User;
+import tripPricer.Provider;
+
+@RestController
+public class TourGuideController {
+
+	@Autowired
+	TourGuideService tourGuideService;
+	
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from TourGuide!";
+    }
+    
+    @RequestMapping("/getLocation") 
+    public String getLocation(@RequestParam String userName) {
+    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
+		return JsonStream.serialize(visitedLocation.location);
+    }
+    
+    //  TODO: Change this method to no longer return a List of Attractions.
+ 	//  Instead: Get the closest five tourist attractions to the user - no matter how far away they are.
+ 	//  Return a new JSON object that contains:
+    	// Name of Tourist attraction, 
+        // Tourist attractions lat/long, 
+        // The user's location lat/long, 
+        // The distance in miles between the user's location and each of the attractions.
+        // The reward points for visiting each Attraction.
+        //    Note: Attraction reward points can be gathered from RewardsCentral
+    @RequestMapping("/getNearbyAttractions") 
+    public String getNearbyAttractions(@RequestParam String userName) {
+    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
+    	return JsonStream.serialize(tourGuideService.getNearByAttractions(visitedLocation));
+    }
+
+    @RequestMapping("/getNearbyAttractionsEdit")
+    public String getNearbyAttractionsEdit(@RequestParam String userName) {
+        NearbyAttractionResult result = new NearbyAttractionResult();
+
+        /* User's Location */
+        VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
+
+        /* Five Closest Attraction */
+        List<Attraction> fiveClosestAttractions = tourGuideService.getFiveClosestAttractions(visitedLocation);
+
+        /* Distance in miles between the user's location and each of the attractions. */
+        List<Double> distances = tourGuideService.getDistancesUserAttractions(fiveClosestAttractions, getUser(userName));
+
+        /* Reward points for visiting each Attraction. */
+        List<Integer> rewards = tourGuideService.getRewardPointsList(fiveClosestAttractions, getUser(userName));
+
+        result.visitedLocation = visitedLocation;
+        result.attractionList = fiveClosestAttractions;
+        result.distances = distances;
+        result.rewards = rewards;
+
+        return JsonStream.serialize(result);
+    }
+
+    /* Pour tester la liste obtenue */
+//    @RequestMapping("/getFiveClosestAttractions")
+//    public List<Attraction> getFiveAttraction(@RequestParam String userName){
+//
+//        VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
+//
+//        return tourGuideService.getFiveClosestAttractions(visitedLocation);
+//    }
+    
+    @RequestMapping("/getRewards") 
+    public String getRewards(@RequestParam String userName) {
+    	return JsonStream.serialize(tourGuideService.getUserRewards(getUser(userName)));
+    }
+    
+    @RequestMapping(value = "/getAllCurrentLocations", produces = { "application/json" })
+    public String getAllCurrentLocations() {
+    	// TODO: Get a list of every user's most recent location as JSON
+    	//- Note: does not use gpsUtil to query for their current location, 
+    	//        but rather gathers the user's current location from their stored location history.
+    	//
+    	// Return object should be the just a JSON mapping of userId to Locations similar to:
+    	//     {
+    	//        "019b04a9-067a-4c76-8817-ee75088c3822": {"longitude":-48.188821,"latitude":74.84371} 
+    	//        ...
+    	//     }
+
+        /* Liste des users */
+        List<User> usersList = tourGuideService.getAllUsers();
+
+        /* User's Location History */
+        StringBuilder result = new StringBuilder();
+        result.append(" {\n");
+        result.append(" ");
+        for (User user : usersList){
+
+            result.append(user.getUserId());
+            result.append(" : ");
+
+            int sizeOfVisitedLocationsList = user.getVisitedLocations().size()-1; /* Dernier élément d'une liste : list.size()-1 */
+            List<VisitedLocation> visitedLocationsList = user.getVisitedLocations();
+            VisitedLocation lastVisitedLocation = visitedLocationsList.get(sizeOfVisitedLocationsList);
+            result.append(JsonStream.serialize(lastVisitedLocation.location));
+            result.append(", \n ");
+        }
+        result.append("}");
+        return result.toString();
+    }
+    
+    @RequestMapping("/getTripDeals")
+    public String getTripDeals(@RequestParam String userName) {
+    	List<Provider> providers = tourGuideService.getTripDeals(getUser(userName));
+    	return JsonStream.serialize(providers);
+    }
+    
+    private User getUser(String userName) {
+    	return tourGuideService.getUser(userName);
+    }
+   
+
+}
---
+++ src/main/java/tourGuide/service/RewardsService.java
@@ -0,0 +1,114 @@
+package tourGuide.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import gpsUtil.GpsUtil;
+import gpsUtil.location.Attraction;
+import gpsUtil.location.Location;
+import gpsUtil.location.VisitedLocation;
+import rewardCentral.RewardCentral;
+import tourGuide.user.User;
+import tourGuide.user.UserReward;
+
+@Service
+public class RewardsService {
+    private static final double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
+
+	// proximity in miles
+    private int defaultProximityBuffer = 10;
+	private int proximityBuffer = defaultProximityBuffer;
+	private int attractionProximityRange = 200;
+	private final GpsUtil gpsUtil;
+	private final RewardCentral rewardsCentral;
+	private List<Attraction> attractions;
+	
+	public RewardsService(GpsUtil gpsUtil, RewardCentral rewardCentral) {
+		this.gpsUtil = gpsUtil;
+		this.rewardsCentral = rewardCentral;
+		this.attractions = gpsUtil.getAttractions();
+	}
+	
+	public void setProximityBuffer(int proximityBuffer) {
+		this.proximityBuffer = proximityBuffer;
+	}
+	
+	public void setDefaultProximityBuffer() {
+		proximityBuffer = defaultProximityBuffer;
+	}
+	
+	public void calculateRewards(User user) {
+		List<VisitedLocation> userLocations = user.getVisitedLocations();
+
+		
+		for(VisitedLocation visitedLocation : userLocations) {
+			for(Attraction attraction : attractions) {
+				if(user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
+					if(nearAttraction(visitedLocation, attraction)) {
+						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
+					}
+				}
+			}
+		}
+	}
+
+	// New method using MultiThreading
+	public void calculateRewards2(List<User> users, int threadNumber) throws InterruptedException {
+		List<Thread> threads = new ArrayList<>();
+		for (int i=0; i<threadNumber; i++){
+			int subListSize = users.size()/threadNumber;
+			List<User> tmp = new ArrayList<>();
+			tmp.addAll(users.subList(i*subListSize, (i+1)*subListSize));
+			Thread myThread = new Thread() {
+				List<User> users;
+				@Override
+				public void run() {
+					for (User user: users) {
+						calculateRewards(user);
+					}
+				}
+				public Thread init(List<User> users){
+					this.users = users;
+					return this;
+				}
+			}.init(tmp);
+			threads.add(myThread);
+		}
+		for (Thread thread : threads) {
+			thread.start();
+		}
+		for (Thread thread : threads) {
+			thread.join();
+		}
+	}
+
+	
+	public boolean isWithinAttractionProximity(Attraction attraction, Location location) {
+		return getDistance(attraction, location) > attractionProximityRange ? false : true;
+	}
+	
+	private boolean nearAttraction(VisitedLocation visitedLocation, Attraction attraction) {
+		return getDistance(attraction, visitedLocation.location) > proximityBuffer ? false : true;
+	}
+	
+	private int getRewardPoints(Attraction attraction, User user) {
+		return rewardsCentral.getAttractionRewardPoints(attraction.attractionId, user.getUserId());
+	}
+	
+	public double getDistance(Location loc1, Location loc2) {
+        double lat1 = Math.toRadians(loc1.latitude);
+        double lon1 = Math.toRadians(loc1.longitude);
+        double lat2 = Math.toRadians(loc2.latitude);
+        double lon2 = Math.toRadians(loc2.longitude);
+
+        double angle = Math.acos(Math.sin(lat1) * Math.sin(lat2)
+                               + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+
+        double nauticalMiles = 60 * Math.toDegrees(angle);
+        double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
+        return statuteMiles;
+	}
+
+}
